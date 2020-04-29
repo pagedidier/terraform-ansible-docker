a093107010c23cb6d62d2302a0177211c3bac2ec
Didier Page <didier.page@heig-vd.ch>
2020-04-29 13:35:19 +0200

    <fix> forgot the -y flag

--- install.sh
+++ install.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 apt update
-apt install git
 apt install software-properties-common -y
 apt-add-repository --yes --update ppa:ansible/ansible
-apt install ansible wget unzip -y
+apt install ansible wget unzip git -y
 
 
 wget https://releases.hashicorp.com/terraform/0.12.24/terraform_0.12.24_linux_386.zip
